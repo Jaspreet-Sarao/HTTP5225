c58e5c219c7f4268dbd285f0124d7aa88238f79e
Jaspreet-Sarao <jaspreetcanadabands@gmail.com>
2025-06-05 20:32:04 -0400

    first push

---
+++ week2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Hello hello!</h1>
+    <?php
+    $firstname= "jaspreet";
+    $lastname= "Kaur";
+    echo"<h1> Hello Hello</h1>;
+    echo "<br><br>";
+    echo"Hello" . $firstname. "" . $lastname";
+    echo "Hello, welcome to Jaspreet\"s world";
+    ?>
+</body>
+</html>
